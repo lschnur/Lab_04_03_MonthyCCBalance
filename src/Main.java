ebf9eeca4b86b6a8803e1c7786a4653a4f6546da
schnurluke <schnurle@mail.uc.edu>
2022-09-15 17:07:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       int card = 5000;
+       double Month1= card* 0.17;
+       double month2= Month1* 0.17;
+        System.out.println("The credit card balance after one month is "+ Month1+ "The credit card balance after 2 months is "+month2);
+
+
+
+
+
+
+
+
+    }
+}
